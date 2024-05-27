Renamed data_handling to data_function to avoid conflict Fixed bug in numerical_differentiation Added function to analyse the sampled data

data_functions/data_creation.py
@@ -5,7 +5,7 @@ import chex
 
 from systems.pendulum_system import PendulumSystem
 from bsm.utils.normalization import Data
-from data_handling.icem_optimizer import powerlaw_psd_gaussian
+from data_functions.icem_optimizer import powerlaw_psd_gaussian
 
 def example_function(t: chex.Array):
     x = jnp.concatenate([jnp.sin(t) * jnp.cos(0.2*t),

differentiators/num_diff/numerical_differentiation.py
@@ -27,8 +27,8 @@ def fitSingleStateTikhonov(t, x, regtype, lambda_, init_offset=0):
     """
     n = t.shape[0]
     x = x - init_offset
-    dt = (t[-1] - t[0]) / n
-    
+    dt = (t[-1] - t[0]) / (n-1)
+
     # Calculate the derivative by solving the least squares problem
     A = jnp.tri(n, n, -1) * dt
     if regtype == 'none':