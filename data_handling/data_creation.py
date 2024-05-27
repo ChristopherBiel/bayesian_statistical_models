97ce69e92d16c18d47e9b843a256a01594628ef0
lenarttreven <lenart.treven44@gmail.com>
2024-05-27 14:40:51 +0200

    incoporate 3d pendulum state

--- data_handling/data_creation.py
+++ data_handling/data_creation.py
@@ -2,22 +2,26 @@ import jax
 import jax.numpy as jnp
 import jax.random as jr
 import chex
+from jax import vmap
 
 from systems.pendulum_system import PendulumSystem
 from bsm.utils.normalization import Data
 from data_handling.icem_optimizer import powerlaw_psd_gaussian
 
+
 def example_function(t: chex.Array):
-    x = jnp.concatenate([jnp.sin(t) * jnp.cos(0.2*t),
-                         0.04*jnp.power(t, 2) + 0.25*t + 1.4,
-                         0.3*jnp.sin(t)], axis=1)
-    return x 
+    x = jnp.concatenate([jnp.sin(t) * jnp.cos(0.2 * t),
+                         0.04 * jnp.power(t, 2) + 0.25 * t + 1.4,
+                         0.3 * jnp.sin(t)], axis=1)
+    return x
+
 
 def example_function_derivative(t: chex.Array):
-    x_dot = jnp.concatenate([jnp.sin(t) * (-0.2) * jnp.sin(0.2*t) + jnp.cos(t) * jnp.cos(0.2*t),
-                             0.08*t + 0.25,
-                             0.3*jnp.cos(t)], axis=1)
+    x_dot = jnp.concatenate([jnp.sin(t) * (-0.2) * jnp.sin(0.2 * t) + jnp.cos(t) * jnp.cos(0.2 * t),
+                             0.08 * t + 0.25,
+                             0.3 * jnp.cos(t)], axis=1)
     return x_dot
+
 
 def sample_example_trajectory(num_points: int,
                               noise_level: float,
@@ -30,18 +34,20 @@ def sample_example_trajectory(num_points: int,
     x = x + noise_level * jr.normal(key=key, shape=x.shape)
     return t, x, x_dot_true
 
+
 def create_example_data(num_trajectories: int,
                         noise_level: float,
                         key: jr.PRNGKey,
                         d_l: float,
-                        min_samples: int=48,
-                        max_samples: int=64):
+                        min_samples: int = 48,
+                        max_samples: int = 64):
     # Create trajectories of varying length with varying number of points
     keys = jr.split(key, num_trajectories)
-    num_points = jr.randint(keys[0], shape=(num_trajectories,), minval=min_samples, maxval=max_samples+1)
-    t, x, x_dot = sample_example_trajectory(max_samples, noise_level, d_l, d_l+(num_points[0]-1)/10, keys[0])
-    for i in range(num_trajectories-1):
-        t_traj, x_traj, x_dot_traj = sample_example_trajectory(max_samples, noise_level, d_l, d_l+(num_points[i+1]-1)/10, keys[i+1])
+    num_points = jr.randint(keys[0], shape=(num_trajectories,), minval=min_samples, maxval=max_samples + 1)
+    t, x, x_dot = sample_example_trajectory(max_samples, noise_level, d_l, d_l + (num_points[0] - 1) / 10, keys[0])
+    for i in range(num_trajectories - 1):
+        t_traj, x_traj, x_dot_traj = sample_example_trajectory(max_samples, noise_level, d_l,
+                                                               d_l + (num_points[i + 1] - 1) / 10, keys[i + 1])
         t = jnp.concatenate([t, t_traj], axis=0)
         x = jnp.concatenate([x, x_traj], axis=0)
         x_dot = jnp.concatenate([x_dot, x_dot_traj], axis=0)
@@ -49,11 +55,12 @@ def create_example_data(num_trajectories: int,
 
 
 def sample_random_pendulum_data(num_points: int,
-                         noise_level: chex.Array | float | None,
-                         key: jr.PRNGKey,
-                         num_trajectories: int | None,
-                         initial_states: chex.Array | None,
-                         input_exponent: float = 3) -> (chex.Array, chex.Array, chex.Array, chex.Array):
+                                noise_level: chex.Array | float | None,
+                                key: jr.PRNGKey,
+                                num_trajectories: int | None,
+                                initial_states: chex.Array | None,
+                                input_exponent: float = 3) -> (chex.Array, chex.Array, chex.Array, chex.Array):
+    state_dim = 3
     if num_trajectories is None and initial_states is None:
         num_trajectories = 1
         initial_states = jnp.array([[-1.0, 0.0, 0.0]])
@@ -61,7 +68,7 @@ def sample_random_pendulum_data(num_points: int,
         num_trajectories = initial_states.shape[0]
     elif initial_states is None:
         initial_states = jnp.array([[-1.0, 0.0, 0.0]] * num_trajectories)
-    
+
     reset_keys = jr.split(key, num_trajectories + 1)
     key = reset_keys[0]
     reset_keys = reset_keys[1:]
@@ -75,27 +82,38 @@ def sample_random_pendulum_data(num_points: int,
     t = jnp.arange(num_points).reshape(-1, 1) * system_state.system_params.dynamics_params.dt
     t = t.T.reshape(num_trajectories, -1, 1)
     assert t.shape == (num_trajectories, num_points, 1)
-    x = jnp.zeros((num_trajectories, num_points, 2))
-    x_dot = jnp.zeros((num_trajectories, num_points, 1))
+
+    x = jnp.zeros((num_trajectories, num_points, state_dim))
+    x_dot = jnp.zeros((num_trajectories, num_points, state_dim))
     # create inputs:
     v_apply = jax.vmap(powerlaw_psd_gaussian, in_axes=(None, None, 0), out_axes=0)
-    action_keys = jr.split(key, num_trajectories+1)
+    action_keys = jr.split(key, num_trajectories + 1)
     key = action_keys[-1]
-    u = v_apply(input_exponent, num_points, action_keys[:-1]).reshape(num_trajectories,num_points,1)
+    u = v_apply(input_exponent, num_points, action_keys[:-1]).reshape(num_trajectories, num_points, 1)
     u = scale_array_to_limits(u, min=-1, max=1)
     for i in range(num_points):
-        system_state = jax.vmap(system.step)(system_state.x_next, u[:,i,:], system_state.system_params)
-        x = x.at[:, i, :].set(system_state.x_next[:, :2])
-        x_dot = x_dot.at[:, i, :].set(system_state.x_next[:, 2:])
+        system_state = jax.vmap(system.step)(system_state.x_next, u[:, i, :], system_state.system_params)
+        x = x.at[:, i, :].set(system_state.x_next)
+        x_compressed = vmap(system.dynamics.get_compressed_state)(system_state.x_next)
+        x_dot_compressed = vmap(system.dynamics.ode)(x_compressed, u[:, i, :],
+                                                     system_state.system_params.dynamics_params)
+        x_dot_compressed = x_dot_compressed.reshape(num_trajectories, 2)
+        x_dot_current = jnp.stack(
+            [-1 * system_state.x_next[..., 1] * x_dot_compressed[..., 0],
+             system_state.x_next[..., 0] * x_dot_compressed[..., 0],
+             x_dot_compressed[..., 1]], axis=1)
 
-    # Transform the derivative (which is theta_dot) to the state space (use chain rule)
-    x_dot = jnp.concatenate([-1 * x[:,:,1].reshape(num_trajectories, -1, 1) * x_dot,
-                             x[:,:,0].reshape(num_trajectories, -1, 1) * x_dot], axis=2)
+        x_dot = x_dot.at[:, i, :].set(x_dot_current)
+
+    # # Transform the derivative (which is theta_dot) to the state space (use chain rule)
+    # x_dot = jnp.concatenate([-1 * x[:, :, 1].reshape(num_trajectories, -1, 1) * x_dot,
+    #                          x[:, :, 0].reshape(num_trajectories, -1, 1) * x_dot], axis=2)
 
     if noise_level is not None:
         x = x + noise_level * jr.normal(key=key, shape=x.shape)
 
     return t, x, u, x_dot
+
 
 def sample_pendulum_with_input(control_input: chex.Array,
                                initial_state: chex.Array = jnp.array([-1.0, 0.0, 0.0]),
@@ -117,51 +135,54 @@ def sample_pendulum_with_input(control_input: chex.Array,
         x_dot = x_dot.at[i, :].set(system_state.x_next[2])
 
     # Transform the derivative (which is theta_dot) to the state space (use chain rule)
-    x_dot = jnp.concatenate([-1 * x[:,:,1].reshape(num_trajectories, -1, 1) * x_dot,
-                             x[:,:,0].reshape(num_trajectories, -1, 1) * x_dot], axis=2)
-    
+    x_dot = jnp.concatenate([-1 * x[:, :, 1].reshape(num_trajectories, -1, 1) * x_dot,
+                             x[:, :, 0].reshape(num_trajectories, -1, 1) * x_dot], axis=2)
+
     if noise_level is not None:
         x = x + noise_level * jr.normal(key=jr.PRNGKey(0), shape=x.shape)
-    
+
     return t, x, x_dot
+
 
 def scale_array_to_limits(array: chex.Array,
                           min: float,
-                          max: float,) -> chex.Array:
+                          max: float, ) -> chex.Array:
     assert min != max, "Min and max values can not be identical"
     min_val = jnp.min(array)
     max_val = jnp.max(array)
-    normalized_array = (array - min_val)/(max_val - min_val)
+    normalized_array = (array - min_val) / (max_val - min_val)
     scaled_arr = normalized_array * (max - min) + min
     return scaled_arr
+
 
 if __name__ == '__main__':
     num_points = 100
     num_trajectories = 48
     key = jr.PRNGKey(0)
     t, x, u, x_dot = sample_random_pendulum_data(num_points=num_points,
-                                              noise_level=0.1,
-                                              key=key,
-                                              num_trajectories=num_trajectories,
-                                              initial_states=None)
+                                                 noise_level=0.1,
+                                                 key=key,
+                                                 num_trajectories=num_trajectories,
+                                                 initial_states=None)
 
     # Plot the data
     import matplotlib.pyplot as plt
+
     print(f"Time shape: {t.shape}")
     print(f"State shape: {x.shape}")
-    fig, axes = plt.subplots(3, min(3,num_trajectories), figsize=(16, 9))
-    for i in range(min(3,num_trajectories)):
+    fig, axes = plt.subplots(3, min(3, num_trajectories), figsize=(16, 9))
+    for i in range(min(3, num_trajectories)):
         axes[0][i].plot(x[i, :, 1], x[i, :, 0])
         axes[0][i].set_xlabel(r'sin($\theta$)')
         axes[0][i].set_ylabel(r'cos($\theta$)')
         axes[0][i].set_title('Trajectory')
-        axes[1][i].plot(t[i,:].reshape(-1), x[i, :, 0], label=r'cos($\theta$)')
-        axes[1][i].plot(t[i,:].reshape(-1), x[i, :, 1], label=r'sin($\theta$)')
-        axes[1][i].plot(t[i,:].reshape(-1), u[i, :, 0], label='Control Input')
+        axes[1][i].plot(t[i, :].reshape(-1), x[i, :, 0], label=r'cos($\theta$)')
+        axes[1][i].plot(t[i, :].reshape(-1), x[i, :, 1], label=r'sin($\theta$)')
+        axes[1][i].plot(t[i, :].reshape(-1), u[i, :, 0], label='Control Input')
         axes[1][i].set_title('State')
         axes[1][i].legend()
         axes[1][i].set_xlabel('Time')
-        axes[2][i].plot(t[i,:].reshape(-1), x_dot[i, :, 0])
+        axes[2][i].plot(t[i, :].reshape(-1), x_dot[i, :, 0])
         axes[2][i].set_title('Derivative')
         axes[2][i].set_xlabel('Time')
     plt.tight_layout()
