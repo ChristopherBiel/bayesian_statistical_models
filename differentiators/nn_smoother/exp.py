99efa77aa05c18aab0e300900da33793ae83aae9
Christopher Biel <christopher.biel@tum.de>
2024-05-28 14:59:10 +0200

    Added features to the nn_smoother experiment

--- differentiators/nn_smoother/exp.py
+++ differentiators/nn_smoother/exp.py
@@ -16,22 +16,27 @@ from data_functions.data_handling import split_dataset
 from data_functions.data_output import plot_derivative_data, plot_data
 
 def experiment(project_name: str = 'LearnDynamicsModel',
-               seed: int=0,
-               num_traj: int = 12,
+               seed: int = 0,
+               num_traj: int = 6,
                sample_points: int = 64,
-               noise_level: float = None,
-               smoother_features: list = [64, 64],
-               dyn_features: list = [128, 128],
+               noise_level: float = 0.01,
+               smoother_feature_size: int = 64,
+               dyn_feature_size: int = 128,
+               smoother_hidden_layers: int = 2,
+               dyn_hidden_layers: int = 2,
                smoother_particles: int = 10,
-               dyn_particles: int = 10,
-               smoother_training_steps: int = 1000,
-               dyn_training_steps: int = 1000,
-               smoother_weight_decay: float = 1e-4,
-               dyn_weight_decay: float = 1e-4,
+               dyn_particles: int = 5,
+               smoother_training_steps: int = 4000,
+               dyn_training_steps: int = 8000,
+               smoother_weight_decay: float = 1e-3,
+               dyn_weight_decay: float = 1e-3,
+               smoother_train_share: float = 0.8,
+               dyn_train_share: float = 0.8,
                smoother_type: str = 'DeterministicEnsemble',
                dyn_type: str = 'DeterministicEnsemble',
                logging_mode_wandb: int = 0,
-               x_src: str = 'smoother'):
+               x_src: str = 'smoother',
+               return_model_state: bool = False,):
     
     # Input checks
     assert num_traj > 0
@@ -45,14 +50,18 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                   num_traj=num_traj,
                   sample_points=sample_points,
                   noise_level=noise_level,
-                  smoother_features=smoother_features,
-                  dyn_features=dyn_features,
+                  smoother_feature_size=smoother_feature_size,
+                  dyn_feature_size=dyn_feature_size,
+                  smoother_hidden_layers=smoother_hidden_layers,
+                  dyn_hidden_layers=dyn_hidden_layers,
                   smoother_particles=smoother_particles,
                   dyn_particles=dyn_particles,
                   smoother_training_steps=smoother_training_steps,
                   dyn_training_steps=dyn_training_steps,
                   smoother_weight_decay=smoother_weight_decay,
                   dyn_weight_decay=dyn_weight_decay,
+                  smoother_train_share=smoother_train_share,
+                  dyn_train_share=dyn_train_share,
                   smoother_type=smoother_type,
                   dyn_type=dyn_type,
                   logging_mode_wandb=logging_mode_wandb,
@@ -98,6 +107,7 @@ def experiment(project_name: str = 'LearnDynamicsModel',
     
 
     # -------------------- Smoother --------------------
+    smoother_features = [smoother_feature_size] * smoother_hidden_layers
     if smoother_type == 'DeterministicEnsemble':
         model = SmootherNet(input_dim=input_dim,
                             output_dim=output_dim,
@@ -107,9 +117,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                             num_particles=dyn_particles,
                             features=smoother_features,
                             bnn_type=DeterministicEnsemble,
-                            train_share=0.6,
+                            train_share=smoother_train_share,
                             num_training_steps=smoother_training_steps,
                             weight_decay=smoother_weight_decay,
+                            return_best_model=True,
+                            eval_frequency=1000,
                             )
     elif smoother_type == 'ProbabilisticEnsemble':
         model = SmootherNet(input_dim=input_dim,
@@ -120,9 +132,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                             num_particles=dyn_particles,
                             features=smoother_features,
                             bnn_type=ProbabilisticEnsemble,
-                            train_share=0.6,
+                            train_share=smoother_train_share,
                             num_training_steps=smoother_training_steps,
                             weight_decay=smoother_weight_decay,
+                            return_best_model=True,
+                            eval_frequency=1000,
                             )
     elif smoother_type == 'DeterministicFSVGDEnsemble':
         model = SmootherNet(input_dim=input_dim,
@@ -133,9 +147,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                             num_particles=dyn_particles,
                             features=smoother_features,
                             bnn_type=DeterministicFSVGDEnsemble,
-                            train_share=0.6,
+                            train_share=smoother_train_share,
                             num_training_steps=smoother_training_steps,
                             weight_decay=smoother_weight_decay,
+                            return_best_model=True,
+                            eval_frequency=1000,
                             )
     elif smoother_type == 'ProbabilisticFSVGDEnsemble':
         model = SmootherNet(input_dim=input_dim,
@@ -146,9 +162,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                             num_particles=dyn_particles,
                             features=smoother_features,
                             bnn_type=ProbabilisticFSVGDEnsemble,
-                            train_share=0.6,
+                            train_share=smoother_train_share,
                             num_training_steps=smoother_training_steps,
                             weight_decay=smoother_weight_decay,
+                            return_best_model=True,
+                            eval_frequency=1000,
                             )
     else:
         raise NotImplementedError(f"Unknown BNN type: {smoother_type}")
@@ -197,9 +215,10 @@ def experiment(project_name: str = 'LearnDynamicsModel',
         inputs = jnp.concatenate([x.reshape(-1, output_dim), u.reshape(-1,control_dim)], axis=-1)
     else:
         raise ValueError(f"No x source {x_src}")
-    outputs = x_dot.reshape(-1, output_dim)
+    outputs = ders.mean.reshape(-1, output_dim)
 
     dyn_data = Data(inputs=inputs, outputs=outputs)
+    dyn_features = [dyn_feature_size] * dyn_hidden_layers
     if dyn_type == 'DeterministicEnsemble':
         dyn_model = BNNStatisticalModel(input_dim=output_dim+control_dim,
                                         output_dim=output_dim,
@@ -209,9 +228,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                                         num_particles=dyn_particles,
                                         features=dyn_features,
                                         bnn_type=DeterministicEnsemble,
-                                        train_share=0.6,
+                                        train_share=dyn_train_share,
                                         num_training_steps=dyn_training_steps,
-                                        weight_decay=dyn_weight_decay
+                                        weight_decay=dyn_weight_decay,
+                                        return_best_model=True,
+                                        eval_frequency=1000,
                                         )
     elif dyn_type == 'ProbabilisticEnsemble':
         dyn_model = BNNStatisticalModel(input_dim=output_dim+control_dim,
@@ -222,9 +243,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                                         num_particles=dyn_particles,
                                         features=dyn_features,
                                         bnn_type=ProbabilisticEnsemble,
-                                        train_share=0.6,
+                                        train_share=dyn_train_share,
                                         num_training_steps=dyn_training_steps,
-                                        weight_decay=dyn_weight_decay
+                                        weight_decay=dyn_weight_decay,
+                                        return_best_model=True,
+                                        eval_frequency=1000,
                                         )
     elif dyn_type == 'DeterministicFSVGDEnsemble':
         dyn_model = BNNStatisticalModel(input_dim=output_dim+control_dim,
@@ -235,9 +258,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                                         num_particles=dyn_particles,
                                         features=dyn_features,
                                         bnn_type=DeterministicFSVGDEnsemble,
-                                        train_share=0.6,
+                                        train_share=dyn_train_share,
                                         num_training_steps=dyn_training_steps,
-                                        weight_decay=dyn_weight_decay
+                                        weight_decay=dyn_weight_decay,
+                                        return_best_model=True,
+                                        eval_frequency=1000,
                                         )
     elif dyn_type == 'ProbabilisticFSVGDEnsemble':
         dyn_model = BNNStatisticalModel(input_dim=output_dim+control_dim,
@@ -248,9 +273,11 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                                         num_particles=dyn_particles,
                                         features=dyn_features,
                                         bnn_type=ProbabilisticFSVGDEnsemble,
-                                        train_share=0.6,
+                                        train_share=dyn_train_share,
                                         num_training_steps=dyn_training_steps,
-                                        weight_decay=dyn_weight_decay
+                                        weight_decay=dyn_weight_decay,
+                                        return_best_model=True,
+                                        eval_frequency=1000,
                                         )
     
     dyn_model_state = dyn_model.init(key)
@@ -265,13 +292,15 @@ def experiment(project_name: str = 'LearnDynamicsModel',
                                    x_dot_est=dyn_preds.mean,
                                    x_dot_est_std=dyn_preds.epistemic_std,
                                    x_dot_smoother=ders.mean,
-                                   x_dot_smoother_std=ders.epistemic_std,
+                                   x_dot_smoother_std=None,
                                    beta=dyn_preds.statistical_model_state.beta,
                                    source='DYN. MODEL',
                                    num_trajectories_to_plot=2,
                                    )
         wandb.log({'dynamics': wandb.Image(fig)})
 
+    if return_model_state:
+        return dyn_model, dyn_model_state
 
 def main(args):
     experiment(project_name=args.project_name,
@@ -279,14 +308,18 @@ def main(args):
                num_traj=args.num_traj,
                sample_points=args.sample_points,
                noise_level=args.noise_level,
-               smoother_features=args.smoother_features,
-               dyn_features=args.dyn_features,
+               smoother_feature_size=args.smoother_feature_size,
+               dyn_feature_size=args.dyn_feature_size,
+               smoother_hidden_layers=args.smoother_hidden_layers,
+               dyn_hidden_layers=args.dyn_hidden_layers,
                smoother_particles=args.smoother_particles,
                dyn_particles=args.dyn_particles,
                smoother_training_steps=args.smoother_training_steps,
                dyn_training_steps=args.dyn_training_steps,
                smoother_weight_decay=args.smoother_weight_decay,
                dyn_weight_decay=args.dyn_weight_decay,
+               smoother_train_share=args.smoother_train_share,
+               dyn_train_share=args.dyn_train_share,
                smoother_type=args.smoother_type,
                dyn_type=args.dyn_type,
                logging_mode_wandb=args.logging_mode_wandb,
@@ -299,17 +332,21 @@ if __name__ == '__main__':
     parser.add_argument('--num_traj', type=int, default=12)
     parser.add_argument('--noise_level', type=float, default=None)
     parser.add_argument('--sample_points', type=int, default=64)
-    parser.add_argument('--smoother_features', type=list, default=[64, 64])
-    parser.add_argument('--dyn_features', type=list, default=[128, 128])
-    parser.add_argument('--smoother_particles', type=int, default=16)
-    parser.add_argument('--dyn_particles', type=int, default=10)
+    parser.add_argument('--smoother_feature_size', type=int, default=64)
+    parser.add_argument('--dyn_feature_size', type=int, default=128)
+    parser.add_argument('--smoother_hidden_layers', type=int, default=2)
+    parser.add_argument('--dyn_hidden_layers', type=int, default=2)
+    parser.add_argument('--smoother_particles', type=int, default=12)
+    parser.add_argument('--dyn_particles', type=int, default=6)
     parser.add_argument('--smoother_training_steps', type=int, default=8000)
     parser.add_argument('--dyn_training_steps', type=int, default=16000)
     parser.add_argument('--smoother_weight_decay', type=float, default=3e-4)
     parser.add_argument('--dyn_weight_decay', type=float, default=3e-4)
+    parser.add_argument('--smoother_train_share', type=float, default=0.8)
+    parser.add_argument('--dyn_train_share', type=float, default=0.8)
     parser.add_argument('--smoother_type', type=str, default='DeterministicEnsemble')
     parser.add_argument('--dyn_type', type=str, default='DeterministicFSVGDEnsemble')
-    parser.add_argument('--logging_mode_wandb', type=int, default=1)
+    parser.add_argument('--logging_mode_wandb', type=int, default=2)
     parser.add_argument('--x_src', type=str, default='data')
     args = parser.parse_args()
     main(args)
